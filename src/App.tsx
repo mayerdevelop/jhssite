7bba530d09117f61bb2de08911736d6a9ee5ab0b
Felipe Mayer <82849496+mayerdevelop@users.noreply.github.com>
2024-12-28 22:03:37 -0300

    feat: melhoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import img from './assets/jhs.png'
 
 const App: React.FC = () => {
   return (
-    <div className="construction-container">
-      <div className="construction-content">
-        <h1>🚧 Site em Construção 🚧</h1>
-        <p>Estamos trabalhando duro para trazer uma experiência incrível para você.</p>
-        <p>Volte em breve!</p>
+    <div className='content'>
+      <img src={img} width={200} />
+      <div className="construction-container">
+        <div className="construction-content">
+          <h1>🚧 Site em Construção 🚧</h1>
+          <p>Estamos trabalhando duro para trazer uma experiência incrível para você.</p>
+          <p>Volte em breve!</p>
+        </div>
       </div>
     </div>
   );
