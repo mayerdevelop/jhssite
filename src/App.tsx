273c770e2d16f79cc9db6058d8027b081248721f
Felipe Mayer <82849496+mayerdevelop@users.noreply.github.com>
2024-12-28 22:15:56 -0300

    feat: melhoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
 import img from './assets/jhs.png'
-
 const App: React.FC = () => {
   return (
-    <div className='content'>
-      <img src={img} width={200} />
-      <div className="construction-container">
-        <div className="construction-content">
-          <h1>🚧 Site em Construção 🚧</h1>
-          <p>Estamos trabalhando duro para trazer uma experiência incrível para você.</p>
-          <p>Volte em breve!</p>
+    <div className="construction-container">
+      <div className="construction-content">
+        <img src={img} width={150} />
+        <h1>🚀 Estamos construindo algo incrível!</h1>
+        <p>
+          Nosso site estará disponível em breve. Estamos dedicados a criar uma experiência única para você.
+        </p>
+        <div className="progress-bar">
+          <div className="progress"></div>
         </div>
+        <p className="footer-text">Agradecemos pela sua paciência! 🙌</p>
       </div>
     </div>
   );
